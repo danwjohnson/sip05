731fe5d78789aceb81e315e77bdc60a1b7147b01
Dan Johnson <djboy71@gmail.com>
2014-02-10 20:22:18 -0600

    Initial commit Finished Recipe 5.3.1

---
+++ src/main/java/com/springinpractice/ch05/mvc/HelloWorldController.java
@@ -0,0 +1,16 @@
+package com.springinpractice.ch05.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloWorldController {
+
+	@RequestMapping("/helloWorldControllerTest")
+	public String showTestPage() {
+		
+		return "helloWorld";
+		
+	} // end showTestPage()
+	
+} // end HelloWorldController class
